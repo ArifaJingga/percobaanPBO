11fa6f1d34332159824ec30d928e719d2b668e76
ArifaJingga <arifajingga23@gmail.com>
2024-11-28 10:29:23 +0800

    first Commit

---
+++ OneDrive/Documents/NetBeansProjects/ContohUASPBO/src/BioskopApp/BioskopApp.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package BioskopApp;
+
+/**
+ *
+ * @author Microsoft
+ */
+public class BioskopApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        new LoginFrame().setVisible(true);
+        
+    }
+    
+}
